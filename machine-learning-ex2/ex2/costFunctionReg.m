2f59e5fd1a16d5c44abbcc2a8b4b21a7e477b6af
Zach G <zeg2@rice.edu>
2017-06-20 07:07:23 -0500

    exercise 2 changes and adds exercize 3

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,36 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+for i = 1:m
+    
+    % Compute hypothesis function.
+    htheta = sigmoid(dot(theta, X(i,:)));
+    
+    % Compute cost function.
+    J = J + (-y(i) * log(htheta) - (1 - y(i)) * log(1 - htheta));
+    
+    % Compute gradient.
+    for j = 1:length(grad)
+        grad(j) = grad(j) + (1 / m) * (htheta - y(i)) * X(i,j);
+    end
+    
+    
+end
 
+J = J / m;
 
+% Regularize the gradient for j > 1.
+for j = 2:length(grad)
+    grad(j) = grad(j) + (lambda / m) * theta(j);
+end
 
+% Regularize the cost function.
+sum = 0;
+for j = 2:length(theta)
+    sum = sum + (theta(j) ^ 2);
+end
+
+J = J + ((lambda / (2 * m)) * sum);
 
 
 % =============================================================
